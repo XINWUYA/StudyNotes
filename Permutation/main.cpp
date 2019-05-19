fc0909e0f08bdc4af3617f72bcab1d55ffa0f304
hawkeye <hawkeye@scu.edu.cn>
2019-05-19 18:57:20 +0800

    prev_permutation, 与next_permutation功能类似，按从大到小对数列数组进行全排序

--- Permutation/main.cpp
+++ Permutation/main.cpp
@@ -2,15 +2,35 @@
 #include <algorithm>
 #include <vector>
 
+/*prev_permutation
+ *  按从大到小对数列数组进行全排序
+ */
+
+ /*next_permutation
+  *  按从小到大对数列数组进行全排序
+  */
+
+#define USE_INCREMENT
+
 int main()
 {
 	std::vector<int> DataArray = { 1,3,4,4,5 };
 	std::sort(DataArray.begin(), DataArray.end());
+
+#ifndef USE_INCREMENT
+	std::reverse(DataArray.begin(), DataArray.end());
+#endif
+
 	do {
 		for (auto i : DataArray)
 			std::cout << i << " ";
 		std::cout << std::endl;
-	} while (std::next_permutation(DataArray.begin(), DataArray.end()));
+	}
+#ifdef USE_INCREMENT
+	while (std::next_permutation(DataArray.begin(), DataArray.end()));
+#else
+	while (std::prev_permutation(DataArray.begin(), DataArray.end()));
+#endif
 
 	std::cout << "After Loop, the next one: " << std::endl;
 	for (auto i : DataArray)
